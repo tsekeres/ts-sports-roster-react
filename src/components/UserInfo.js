6b3fe7746f9897da00dde6a50f54abb34cf79914
tsekeres <twsekeres@gmail.com>
2021-05-10 17:57:18 -0500

    sussing the home page

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -10,9 +10,9 @@ import PropTypes from 'prop-types';
 
 const UserInfoCard = ({ user }) => (
   <Card body>
-    <CardImg top width="100%" src={user?.profileImage} alt="Card image cap" />
+    <CardImg top width="100%" src={user.profileImage} alt="Card image cap" />
     <CardBody>
-      <CardTitle tag="h5">{user?.fullName}</CardTitle>
+      <CardTitle tag="h5">{user.fullName}</CardTitle>
       <CardSubtitle tag="h6" className="mb-2 text-muted">Manager</CardSubtitle>
     </CardBody>
   </Card>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import UserInfoCard from '../components/UserInfo';
+// import UserInfoCard from '../components/UserInfo';
 
 function Home({ user }) {
   return (
     <div className="user-welcome">
       <h1>Welcome to TSSports</h1>
       <hr></hr>
-      <div className="user-data"><UserInfoCard user={user} /></div>
+      {/* <div className="user-data"><UserInfoCard user={user} /></div> */}
       <hr></hr>
-      { user ? <h2>Your team awaits</h2> : <h2>Please Log In</h2>}
+      {user ? <h2>Your team awaits</h2> : <h2>Please Log In</h2>}
     </div>
   );
 }
